91f040fe42c73f7fe507bdd63ae861e7d1e799db
Lilima Pradhan <24977719+lilimapradhan9@users.noreply.github.com>
2021-05-08 09:30:43 +0530

    Add characterization test for text statement

--- src/test/java/com/thoughtworks/movierental/CustomerTest.java
+++ src/test/java/com/thoughtworks/movierental/CustomerTest.java
@@ -5,11 +5,19 @@ import org.junit.Test;
 
 public class CustomerTest {
     @Test
-    public void test() {
+    public void shouldGenerateTextStatement() {
+        Customer customer = new Customer("Lilima");
+        customer.addRental(new Rental(new Movie("Iron Man", Movie.REGULAR), 4));
+        customer.addRental(new Rental(new Movie("Frozen", Movie.CHILDRENS), 4));
+        customer.addRental(new Rental(new Movie("Black Widow", Movie.NEW_RELEASE), 4));
 
-        int expected = 3;
+        String statement = customer.statement();
 
-        Assert.assertEquals(expected, 1 + 2);
+        Assert.assertEquals("Rental Record for Lilima\n" +
+                "\tIron Man\t5.0\n" +
+                "\tFrozen\t3.0\n" +
+                "\tBlack Widow\t12.0\n" +
+                "Amount owed is 20.0\n" +
+                "You earned 4 frequent renter points", statement);
     }
-
 }
